fc58d1337c26922ad1b92c4d90bde2e02f60864d
BugInMyHEAD <buginmyhead@gmail.com>
2026-02-08 16:55:05 +0900

    Fix comments

--- src/main/java/com/buginmyhead/tools/kotlin/graph/ImmutableGraph.kt
+++ src/main/java/com/buginmyhead/tools/kotlin/graph/ImmutableGraph.kt
@@ -1,8 +1,14 @@
 package com.buginmyhead.tools.kotlin.graph
 
+import com.buginmyhead.tools.kotlin.graph.Graph.Companion.toGraph
+
 /**
  * It is a simple wrapper over a [Graph] to mark it as immutable.
  * Actual immutability can be achieved by encapsulating access to the mutable delegate.
+ *
+ * @see [Graph.toGraph]
+ * @see [AcyclicGraph]
+ * @see [Tree]
  */
 interface ImmutableGraph<N, W> : Graph<N, W> {
 
