4309c4d1703ea8c10fe0cd08edb857ee6ae0c032
BugInMyHEAD <buginmyhead@gmail.com>
2026-03-16 06:39:48 +0900

    Comment

--- src/main/java/com/buginmyhead/tools/kotlin/statemachine/TransitionFunction.kt
+++ src/main/java/com/buginmyhead/tools/kotlin/statemachine/TransitionFunction.kt
@@ -10,8 +10,15 @@ fun interface TransitionFunction<S : T, T : TypeSafeBroker.Key<*>> {
      */
     fun onEvent(states: List<T>, root: S, event: Any): Transition<S>
 
+    /**
+     * The default implementation of [TransitionFunction]
+     *  that creates a [Scope] and passes it to [Scope.onEvent].
+     */
     fun interface WithScope<S : T, T : TypeSafeBroker.Key<*>> : TransitionFunction<S, T> {
 
+        /**
+         * @see TransitionFunction.onEvent
+         */
         fun Scope.onEvent(states: List<T>, root: S, event: Any): S
 
         override fun onEvent(states: List<T>, root: S, event: Any): Transition<S> {
